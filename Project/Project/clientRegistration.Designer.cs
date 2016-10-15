46804b65c7f4fb175150b36a5990195cda991d0e
JohnVanSchalkwyk <johnvanschalkwyk@gmail.com>
2016-10-15 20:32:07 +0200

    Cosmetic Changes

--- Project/Project/clientRegistration.Designer.cs
+++ Project/Project/clientRegistration.Designer.cs
@@ -37,29 +37,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Location = new System.Drawing.Point(35, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 20);
+            this.label1.Size = new System.Drawing.Size(370, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome new Client!";
+            this.label1.Text = "Your new account is only a few steps away ...";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 66);
+            this.label2.Location = new System.Drawing.Point(94, 39);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(248, 13);
+            this.label2.Size = new System.Drawing.Size(245, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Please click on the begin button to start registering!";
+            this.label2.Text = "Please click on Begin to become part of the family ";
             // 
             // btnStartRegister
             // 
             this.btnStartRegister.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnStartRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStartRegister.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnStartRegister.Location = new System.Drawing.Point(12, 105);
+            this.btnStartRegister.Location = new System.Drawing.Point(12, 69);
             this.btnStartRegister.Name = "btnStartRegister";
-            this.btnStartRegister.Size = new System.Drawing.Size(260, 49);
+            this.btnStartRegister.Size = new System.Drawing.Size(411, 49);
             this.btnStartRegister.TabIndex = 2;
             this.btnStartRegister.Text = "Begin!";
             this.btnStartRegister.UseVisualStyleBackColor = false;
@@ -69,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 200);
+            this.ClientSize = new System.Drawing.Size(435, 126);
             this.Controls.Add(this.btnStartRegister);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
