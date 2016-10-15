79ea6366fb86dbd404fa96a20ababf6fb5249e7e
aetherZA <darrenjohnadams@outlook.com>
2016-10-15 20:42:47 +0200

    Client DataGrid is complete for summary.

--- Project/Project/ClientForm.cs
+++ Project/Project/ClientForm.cs
@@ -150,9 +150,10 @@ namespace Project
             lblCellNum.Text = cellnum;
             lblWelcome.Text = "Welcome, " + fname + " " + lname;
 
-            //Load All Values into truck
+           
 
-            string getTrucks = "SELECT * FROM BookingTruck WHERE client_id = @clientid AND booking_arrival_date > @currentDate" ;
+            //Load All Values into truck
+            string getTrucks = "SELECT BookingTruck.booking_date_made AS Creation_Date, BookingTruck.booking_departure_date AS Departure_Date, BookingTruck.booking_arrival_date AS Arrival_Date, BookingTruck.departure_street_number + ' ' + BookingTruck.departure_street_name + ', ' + BookingTruck.departure_street_area + ', ' + BookingTruck.departure_city AS Source_Address, BookingTruck.arrival_street_number + ' ' + BookingTruck.arrival_street_name + ', ' + BookingTruck.arrival_street_area + ', ' + BookingTruck.arrival_city AS Destination_Address, Trucks.truck_registration AS Truck, Staff.firstname + ' ' + Staff.lastname AS Staff_Member, BookingGoods.goods_type AS Goods,BookingTruck.booking_notes AS Notes FROM BookingTruck,Trucks,BookingGoods,Staff WHERE BookingTruck.client_id = @clientid AND BookingTruck.booking_arrival_date >= @currentDate AND BookingTruck.truck_id = Trucks.truck_id AND BookingTruck.staff_id = Staff.staff_id AND BookingTruck.goods_id = BookingGoods.goods_id";
             string current = DateTime.Today.ToString("yyyy-MM-dd");
             try
             {
@@ -172,13 +173,199 @@ namespace Project
             {
                 MessageBox.Show("Error: " + error.Message);
             }
+
+            string getTrucks2 = "SELECT BookingTruck.booking_date_made AS Creation_Date, BookingTruck.booking_departure_date AS Departure_Date, BookingTruck.booking_arrival_date AS Arrival_Date, BookingTruck.departure_street_number + ' ' + BookingTruck.departure_street_name + ', ' + BookingTruck.departure_street_area + ', ' + BookingTruck.departure_city AS Source_Address, BookingTruck.arrival_street_number + ' ' + BookingTruck.arrival_street_name + ', ' + BookingTruck.arrival_street_area + ', ' + BookingTruck.arrival_city AS Destination_Address, Trucks.truck_registration AS Truck, Staff.firstname + ' ' + Staff.lastname AS Staff_Member, BookingGoods.goods_type AS Goods,BookingTruck.booking_notes AS Notes FROM BookingTruck,Trucks,BookingGoods,Staff WHERE BookingTruck.client_id = @clientid AND BookingTruck.booking_arrival_date < @currentDate AND BookingTruck.truck_id = Trucks.truck_id AND BookingTruck.staff_id = Staff.staff_id AND BookingTruck.goods_id = BookingGoods.goods_id";
+            string current2 = DateTime.Today.ToString("yyyy-MM-dd");
+            try
+            {
+                cmd.Connection = conn;
+                cmd.Parameters["@currentDate"].Value = current2;
+                cmd.CommandText = getTrucks2;
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataTable ds = new DataTable();
+                adapt.Fill(ds);
+                dataGridInactiveBook.DataSource = ds;
+
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
             finally
             {
                 conn.Close();
             }
 
+
+            //Load Old Values into truck
+
+        }
+        public string getTruckName(int id)
+        {
+            SqlConnection connection2 = new SqlConnection(Globals.DBConn);
+            SqlCommand command4 = new SqlCommand();
+            SqlDataReader reader2;
+            string retName = "";
+            string query = "SELECT * FROM Trucks WHERE truck_id = @id";
+            command4.Parameters.AddWithValue("@id", id);
+            command4.Connection = connection2;
+            command4.CommandText = query;
+
+            try
+            {
+                connection2.Open();
+                reader2 = command4.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    retName = reader2["truck_registration"].ToString();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                connection2.Close();
+            }
+
+
+            return retName;
         }
 
+        public string getStaffName(int id)
+        {
+            SqlConnection connection2 = new SqlConnection(Globals.DBConn);
+            SqlCommand command4 = new SqlCommand();
+            SqlDataReader reader2;
+            string retName = "";
+            string query = "SELECT * FROM Staff WHERE staff_id = @id";
+            command4.Parameters.AddWithValue("@id", id);
+            command4.Connection = connection2;
+            command4.CommandText = query;
+
+            try
+            {
+                connection2.Open();
+                reader2 = command4.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    retName = reader2["firstname"] + " " + reader2["lastname"];
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                connection2.Close();
+            }
+
+
+            return retName;
+        }
+
+        public string getGoodsName(int id)
+        {
+            SqlConnection connection2 = new SqlConnection(Globals.DBConn);
+            SqlCommand command4 = new SqlCommand();
+            SqlDataReader reader2;
+            string retName = "";
+            string query = "SELECT * FROM BookingGoods WHERE goods_id = @id";
+            command4.Parameters.AddWithValue("@id", id);
+            command4.Connection = connection2;
+            command4.CommandText = query;
+
+            try
+            {
+                connection2.Open();
+                reader2 = command4.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    retName = reader2["goods_type"].ToString();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                connection2.Close();
+            }
+
+
+            return retName;
+        }
+
+
+        public string getDriverName(int id)
+        {
+            SqlConnection connection2 = new SqlConnection(Globals.DBConn);
+            SqlCommand command2 = new SqlCommand();
+            SqlDataReader reader2;
+            int staffid = -1;
+            string query = "SELECT * FROM TruckDrivers WHERE driver_id = @id";
+            command2.Parameters.AddWithValue("@id", id);
+
+            command2.Connection = connection2;
+            command2.CommandText = query;
+
+            try
+            {
+                connection2.Open();
+                reader2 = command2.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    staffid = (Int32)reader2["staff_id"];
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                connection2.Close();
+            }
+
+
+
+            //Next Query
+            string query2 = "SELECT * FROM Staff WHERE staff_id = @id";
+            command2.Parameters["@id"].Value = staffid;
+            command2.Connection = connection2;
+            command2.CommandText = query2;
+            string retName = "";
+            try
+            {
+                connection2.Open();
+                reader2 = command2.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    retName = reader2["firstname"] + " " + reader2["lastname"];
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                connection2.Close();
+            }
+
+
+            return retName;
+        }
         public Image byteArrayToImage(byte[] byteArrayIn)
         {
             Image returnImage = null;
