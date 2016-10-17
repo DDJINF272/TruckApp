1f24d2d3461e8928c6e8a7559105bc35c9b521b2
aetherZA <darrenjohnadams@outlook.com>
2016-10-17 15:57:30 +0200

    Email Is Successfult Updated

---
+++ Project/Project/emailClient.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Project
+{
+    partial class emailClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbClients = new System.Windows.Forms.ComboBox();
+            this.txtBxSubject = new System.Windows.Forms.TextBox();
+            this.txtBxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSendMail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbClients
+            // 
+            this.cmbClients.FormattingEnabled = true;
+            this.cmbClients.Location = new System.Drawing.Point(124, 70);
+            this.cmbClients.Name = "cmbClients";
+            this.cmbClients.Size = new System.Drawing.Size(273, 21);
+            this.cmbClients.TabIndex = 0;
+            // 
+            // txtBxSubject
+            // 
+            this.txtBxSubject.Location = new System.Drawing.Point(124, 123);
+            this.txtBxSubject.Name = "txtBxSubject";
+            this.txtBxSubject.Size = new System.Drawing.Size(273, 20);
+            this.txtBxSubject.TabIndex = 1;
+            // 
+            // txtBxMessage
+            // 
+            this.txtBxMessage.Location = new System.Drawing.Point(124, 173);
+            this.txtBxMessage.Multiline = true;
+            this.txtBxMessage.Name = "txtBxMessage";
+            this.txtBxMessage.Size = new System.Drawing.Size(273, 191);
+            this.txtBxMessage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Client";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Subject";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message";
+            // 
+            // btnSendMail
+            // 
+            this.btnSendMail.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSendMail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSendMail.Location = new System.Drawing.Point(124, 381);
+            this.btnSendMail.Name = "btnSendMail";
+            this.btnSendMail.Size = new System.Drawing.Size(274, 36);
+            this.btnSendMail.TabIndex = 6;
+            this.btnSendMail.Text = "Send Now!";
+            this.btnSendMail.UseVisualStyleBackColor = false;
+            this.btnSendMail.Click += new System.EventHandler(this.btnSendMail_Click);
+            // 
+            // emailClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 462);
+            this.Controls.Add(this.btnSendMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxMessage);
+            this.Controls.Add(this.txtBxSubject);
+            this.Controls.Add(this.cmbClients);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "emailClient";
+            this.Text = "Send An Email";
+            this.Load += new System.EventHandler(this.emailClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbClients;
+        private System.Windows.Forms.TextBox txtBxSubject;
+        private System.Windows.Forms.TextBox txtBxMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSendMail;
+    }
+}
---
+++ Project/Project/emailClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Project
+{
+    public partial class emailClient : Form
+    {
+        SqlConnection conn = new SqlConnection(Globals.DBConn);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+        public emailClient()
+        {
+            InitializeComponent();
+        }
+
+        private void emailClient_Load(object sender, EventArgs e)
+        {
+            string arg = "SELECT Clients.client_firstname + ' ' + Clients.client_lastname AS Name,ClientLogin.clientMail AS Email FROM Clients,ClientLogin WHERE Clients.client_login = ClientLogin.clientLogin_id";
+            try
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = arg;
+                conn.Open();
+                reader = cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+
+                    cmbClients.Items.Add(new ComboBoxItems { name = reader["Name"].ToString(), value = reader["Email"].ToString() });
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnSendMail_Click(object sender, EventArgs e)
+        {
+            if(txtBxMessage.Text == "" || txtBxSubject.Text == "")
+            {
+                MessageBox.Show("Please provide information to send.");
+            }
+            else
+            {
+                ComboBoxItems item = (ComboBoxItems)cmbClients.SelectedItem;
+                string toSend = item.value;
+                
+                string subject = txtBxSubject.Text;
+                string message = txtBxMessage.Text;
+
+                
+                email.sendMail(toSend, message, subject);
+            }
+        }
+    }
+}
